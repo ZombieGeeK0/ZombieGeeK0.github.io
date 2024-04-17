14325e57292d3a0e555cf5721aa43ba559aa2231
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-04-17 20:29:17 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function guardarUsuario() {
+  var usuario = document.getElementById("userInput").value;  
+  var contacto = document.getElementById("contactInput").value;
+  window.location.href = "menu.html";
+}
