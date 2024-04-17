6e2396536053e2ecf718818d0f84ecbe6123235b
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-04-17 11:49:37 +0200

    Create form-script.js

---
+++ form-script.js
@@ -0,0 +1,9 @@
+const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
+            function cambiaTema(ev){
+                if(ev.target.checked){
+                    document.documentElement.setAttribute('tema', 'light');
+                } else {
+                    document.documentElement.setAttribute('tema', 'dark');
+                }
+            }
+            colorSwitch.addEventListener('change', cambiaTema);
