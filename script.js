821577649638f737891267cd17af7a8d88f32c37
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-04-18 16:38:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function guardarUsuario() {
-  var usuario = document.getElementById("userInput").value;  
+  var usuario = document.getElementById("userInput").value;
   var contacto = document.getElementById("contactInput").value;
   window.location.href = "menu.html";
+}
+
+function verificarContrasena() {
+  var contrasenaIngresada = document.getElementById("contrasenaInput").value;
+  var contrasenaCorrecta = "h3ll0w0rld";
+
+  if (contrasenaIngresada === contrasenaCorrecta) {
+    document.getElementById("formulario").style.display = "none";
+    window.location.href = "user-0000.html";
+  } else {
+    document.getElementById("mensajeError").innerText =
+      "Contraseña incorrecta. Inténtalo de nuevo.";
+  }
 }
