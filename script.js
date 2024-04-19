5aedaa6b124320cd78f55c8fa79c2929ad7c6a5b
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-04-19 10:26:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-function guardarUsuario() {
-  var usuario = document.getElementById("userInput").value;
-  var contacto = document.getElementById("contactInput").value;
-  window.location.href = "menu.html";
-}
-
 function verificarContrasena() {
-  var contrasenaIngresada = document.getElementById("contrasenaInput").value;
-  var contrasenaCorrecta = "h3ll0w0rld";
+	var contrasenaIngresada = document.getElementById("contrasenaInput").value;
+	var contrasenaCorrecta = "wikipedia";
 
-  if (contrasenaIngresada === contrasenaCorrecta) {
-    document.getElementById("formulario").style.display = "none";
-    window.location.href = "user-0000.html";
-  } else {
-    document.getElementById("mensajeError").innerText =
-      "Contraseña incorrecta. Inténtalo de nuevo.";
-  }
+	if (contrasenaIngresada === contrasenaCorrecta) {
+		document.getElementById("formulario").style.display = "none";
+		window.location.href = "dashboard.html";
+	} else {
+		document.getElementById("mensajeError").innerText =
+			"Contraseña incorrecta. Inténtalo de nuevo.";
+	}
 }
