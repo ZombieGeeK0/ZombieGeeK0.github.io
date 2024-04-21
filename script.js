d4772e755e9da104369f6aa39ef3e2b0e1fe3b17
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-04-21 20:48:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function verificarContrasena() {
 			"Contraseña incorrecta. Inténtalo de nuevo.";
 	}
 }
+
+document.getElementById("discord-button").addEventListener("click", function() {
+    window.location.href = "https://discord.com/users/1112290766631477248";
+});
+  
+document.getElementById("email-button").addEventListener("click", function() {
+    window.location.href = "mailto:3xpl017.contact@proton.me";
+});
+  
+document.getElementById("x-button").addEventListener("click", function() {
+    window.location.href = "https://twitter.com/?lang=es";
+});
+
+document.getElementById("github-button").addEventListener("click", function() {
+    window.location.href = "https://github.com/ZombieGeeK0";
+});
